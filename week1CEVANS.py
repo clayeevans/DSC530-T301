bd144da1ed0c01bec5dae4bcf2400a39186b1689
clayeevans <58152676+clayeevans@users.noreply.github.com>
2020-05-31 18:22:25 -0400

    Add files via upload

---
+++ week1CEVANS.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 18:04:51 2020
+
+@author: Clayton Evans
+
+DSC530 Week 1 Assignment
+"""
+
+print("Hello World.")
+
+print(6 + 3)
+
+print(6 - 3)
+
+print(6 * 3)
+
+print(6 / 3)
+
+print("Pop" + "-" + "tarts")
+
+simpleList = [1,2,3,4]
+print(simpleList)
+
+simpleList.append(5)
+print(simpleList)
+
+simpleTuple = (1,2,3,4)
+print(simpleTuple)
